bdcf4449b95eff02de83aaaac23de7a919d500b8
Cyle <cylegage@gmail.com>
2013-08-16 14:15:56 -0400

    - testing more with the test script, will flesh out test.php to test all features

--- test.php
+++ test.php
@@ -7,15 +7,35 @@ require('sigil.php');
 $sigil = new SIGIL();
 
 
-echo '<h3>making new node:</h3>';
+echo '<h3>making new nodes:</h3>';
 
-$result = $sigil->newNode("API-created node", 0, 0, 0);
+$result = $sigil->newNode("node 1", 4, 4, 0, array( 'welp' => 'huh' ));
 var_dump($result);
 if ($result == false) {
 	var_dump($sigil->last_error);
 	die();
+} else {
+	$first_node_id = $result;
+}
+
+$result = $sigil->newNode("node 2", 6, 9, 0);
+var_dump($result);
+if ($result == false) {
+	var_dump($sigil->last_error);
+	die();
+} else {
+	$second_node_id = $result;
 }
 
 echo '<h3>making new connection:</h3>';
+
+$result = $sigil->newConnection("connection 1", $first_node_id, $second_node_id);
+var_dump($result);
+if ($result == false) {
+	var_dump($sigil->last_error);
+	die();
+} else {
+	$first_conn_id = $result;
+}
 
 ?>
